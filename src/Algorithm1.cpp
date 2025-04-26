9e333d6d2223d698270858be73dbaf40d4ba2c76
Adi Lazarovici <lazarovici@mail.tau.ac.il>
2025-04-26 19:08:18 +0300

    more documentation

--- src/Algorithm1.cpp
+++ src/Algorithm1.cpp
@@ -29,9 +29,9 @@ std::vector<Direction::Value> Algorithm1::computeBFS(const Board& board, const T
         auto key = std::make_pair(current.pos.x, current.pos.y);
         if (visited.count(key)) continue;
         visited.insert(key);
-
+        // checking if the next step in the queue can lead to shooting the enemy directly
         Tank fakeTank(self.getSymbol(), self.getAmmo(), self.getDirection(), current.pos,
-                      self.getShootingStatus(), self.getBackwardStatus());
+                      self.getShootingStatus(), self.getBackwardStatus()); // a fake tank to do the testing on
 
         if (canShoot(fakeTank, enemy, board)) {
             if (current.path.size() < shortestLength) {
@@ -40,7 +40,7 @@ std::vector<Direction::Value> Algorithm1::computeBFS(const Board& board, const T
             }
             continue;
         }
-
+        // if the next step can't lead to a shooting position
         for (int i = 0; i < 8; ++i) {
             Direction::Value dir = static_cast<Direction::Value>(i);
             Position next = current.pos + Direction(dir).toVector();
@@ -49,7 +49,7 @@ std::vector<Direction::Value> Algorithm1::computeBFS(const Board& board, const T
                 continue;
 
             if (visited.count({next.x, next.y})) continue;
-
+            // if there's a mine, don't go to it
             const CellSlot& slot = board.getSlot(next.x, next.y);
             if (slot.getMine()) continue;
 
@@ -58,16 +58,16 @@ std::vector<Direction::Value> Algorithm1::computeBFS(const Board& board, const T
             q.push({next, newPath});
         }
     }
-
     return bestPath;
 }
 
 Action Algorithm1::nextAction(const Board& board, const Tank& self, const Tank& enemy) {
+    // if threteand by shells, escape as self defense
     if (isThreatenedByShells(board, self.getPosition())) {
         Logger::debug("Player 1: Threatened By Shells");
         return moveIfThreatened(board, self);
     }
-
+    // if there is no ammo left, he can't be agressive so he does nothing if he is not threteand
     if (self.getAmmo() == 0) {
         Logger::debug("Player 1: No ammo");
         return Action(ActionType::None);
@@ -75,13 +75,13 @@ Action Algorithm1::nextAction(const Board& board, const Tank& self, const Tank& 
     Logger::debug("Player 1: Checking shooting condition - canShoot: " +
               std::string(canShoot(self, enemy, board) ? "true" : "false") +
               ", shootingStatus: " + std::to_string(self.getShootingStatus()));
-
+    // if he has a direct path to shoot and also is not in cooldown, he shoots
     if (canShoot(self, enemy, board) && self.getShootingStatus() == 0) {
         Logger::debug("Player 1: Enemy in direction " + std::to_string(self.getDirection().getDirection()) + ". Shooting now.");
         triedPathWithoutSuccess = false;
         return Action(ActionType::Shoot);
     }
-
+    // checks if he can rotate towards a shooting position
     for (int dir = 0; dir < 8; ++dir) {
         if (self.getDirection().getDirection() == dir) continue;
         Direction d = Direction(static_cast<Direction::Value>(dir));
@@ -95,9 +95,9 @@ Action Algorithm1::nextAction(const Board& board, const Tank& self, const Tank& 
         }
     }
 
-    bool enemyMoved = !(enemy.getPosition() == lastEnemyPos);
-    bool shouldComputeBFS = currentPath.empty() || enemyMoved || triedPathWithoutSuccess;
-
+    bool enemyMoved = !(enemy.getPosition() == lastEnemyPos); // checks if the enemy tank moved in the last step
+    bool shouldComputeBFS = currentPath.empty() || enemyMoved || triedPathWithoutSuccess; // checks if he doesn't have a BFS computed or the enemy moved, or the current path in the BFS blocked
+    // computes BFS again - doesn't do it every turn - only when needed
     if (shouldComputeBFS) {
         std::string reason = currentPath.empty() ? "empty" : (enemyMoved ? "enemy moved" : "triedPathWithoutSuccess");
         Logger::debug("Player 1: Computing BFS (reason: " + reason + ")");
@@ -105,7 +105,7 @@ Action Algorithm1::nextAction(const Board& board, const Tank& self, const Tank& 
         triedPathWithoutSuccess = currentPath.empty();
         lastEnemyPos = enemy.getPosition();
     }
-
+    // if there is no path for the enemy tank, he is stuck and can't move he shoots if possible or rotating to the right if not
     if (currentPath.empty()) {
         if (!canShoot(self, enemy, board)) {
             Logger::debug("Player 1: Stuck, rotating randomly");
@@ -123,20 +123,20 @@ Action Algorithm1::nextAction(const Board& board, const Tank& self, const Tank& 
         triedPathWithoutSuccess = false;
         return Action(ActionType::Shoot);
     }
-
+    // if the BFS is computed correctly, do the next step in the BFS path
     Direction::Value targetDir = currentPath.front();
     if (self.getDirection().getDirection() == targetDir) {
         Position nextPos = self.getPosition() + Direction(targetDir).toVector();
 
         const CellSlot& slot = board.getSlot(nextPos.x, nextPos.y);
-
+        // won't go to a cell that has a mine
         if (slot.getMine()) {
             Logger::debug("Player 1: Mine ahead – aborting move and resetting path");
             currentPath.clear();
             triedPathWithoutSuccess = true;
             return Action(ActionType::None);
         }
-
+        // if he has a wall in his BFS path, he shoots it in order to take it down
         if (slot.getWall()) {
             if (self.getShootingStatus() == 0 && self.getAmmo() > 0) {
                 Logger::debug("Player 1: Wall ahead – shooting it");
@@ -145,13 +145,13 @@ Action Algorithm1::nextAction(const Board& board, const Tank& self, const Tank& 
             }
             return Action(ActionType::None);
         }
-
+        // after doing the path, remove it from the current path steps and move forward
         currentPath.erase(currentPath.begin());
         Logger::debug("Player 1: Moving forward to (" + std::to_string(nextPos.x) + "," + std::to_string(nextPos.y) + ")");
         triedPathWithoutSuccess = false;
         return Action(ActionType::MoveForward);
     }
-
+    // if he isn't able to do anything, just rotate and maybe it will help in the next steps
     Logger::debug("Player 1: Rotating from " + std::to_string(self.getDirection().getDirection()) + " to " + std::to_string(targetDir));
     triedPathWithoutSuccess = false;
     return Action(rotateTowards(self.getDirection().getDirection(), targetDir));
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -8,7 +7,7 @@
 #include "Mine.h"
 #include "GameManager.h"
 #include "Logger.h"
-#include <set> // בתחילת הקובץ אם עדיין לא מופיע
+#include <set> 
 
 
 
@@ -22,8 +21,9 @@ int main(int argc, char* argv[]) {
         std::cerr << "Error: Could not open file!" << std::endl;
         return 1;
     }
-    Logger::init("log_config.txt");  // הפעלת לוגים על בסיס הקונפיג
+    Logger::init("log_config.txt");
 
+    // loading the board from the input file
     int width, height;
     file >> width >> height;
     file.ignore();// \n
@@ -50,14 +50,12 @@ int main(int argc, char* argv[]) {
                     board.addObject(new Tank('1', 16, Direction(Direction::L), Position(x, y)), x, y);
                     tank1=true;
                 }
-
             }
             else if (c == '2') {
                 if(tank2==false){
                     board.addObject(new Tank('2', 16, Direction(Direction::R), Position(x, y)), x, y);
                     tank2=true;
                 }
-
             }
             else{
                 continue;
@@ -65,105 +63,37 @@ int main(int argc, char* argv[]) {
     }
     }
     file.close();
-    std::cout << "✅ Board loaded successfully." << std::endl;
-    std::cout << "📦 Objects on board:" << std::endl;
 
-    for (int y = 0; y < board.getHeight(); ++y) {
-        for (int x = 0; x < board.getWidth(); ++x) {
-            const CellSlot& slot = board.getSlot(x, y);
-            const auto& objects = slot.getAll();
-            for (Cell* obj : objects) {
-                std::cout << "[" << x << "," << y << "] "
-                        << obj->toString() << std::endl;
-            }
-        }
-    }
-
-
-// for (int y = 0; y < board.getHeight(); ++y) {
-//     for (int x = 0; x < board.getWidth(); ++x) {
-//         const CellSlot& slot = board.getSlot(x, y);
-//         const std::vector<Cell*>& objects = slot.getAll();
-
-//         if (!objects.empty()) {
-//             Cell* obj = objects.back();
-//             char symbol = obj->getSymbol();
-//             std::cout << symbol;
-
-//             // בדיקה: איזה אובייקט זה באמת?
-//             if (dynamic_cast<Tank*>(obj)) {
-//                 std::cout << "(T)";
-//             } else if (dynamic_cast<Wall*>(obj)) {
-//                 std::cout << "(W)";
-//             } else if (dynamic_cast<Mine*>(obj)) {
-//                 std::cout << "(M)";
-//             } else if (dynamic_cast<Shell*>(obj)) {
-//                 std::cout << "(S)";
-//             } else {
-//                 std::cout << "(?)";  // משהו לא צפוי
-//             }
-
-//         } else {
-//             std::cout << ' ';
-//         }
-//     }
-//     std::cout << '\n';
-// }
+    // creating the GameManager and starting the game
     GameManager game(board, std::string(argv[1]));
     game.gameLoop();
 
-    // 📤 הדפסת קובץ פלט
+    // writing to the output file the entire game turns
     std::string outputFileName = "output_" + std::string(argv[1]);
     std::ifstream outFile(outputFileName);
     if (!outFile) {
         std::cerr << "Failed to open game_output.txt" << std::endl;
         return 1;
     }
-
-    std::cout << "\n📄 Game Output:\n";
-    std::string line;
-    while (std::getline(outFile, line)) {
-        std::cout << line << std::endl;
-    }
-
-
     outFile.close();
-        // 📤 הדפסת debug_log.txt
-    // std::ifstream debugFile("debug_log.txt");
-    // if (!debugFile) {
-    //     std::cerr << "Failed to open debug_log.txt" << std::endl;
-    //     return 1;
-    // }
 
-    // std::cout << "\n🪵 Debug Log:\n";
-    // std::string debugLine;
-    // while (std::getline(debugFile, debugLine)) {
-    //     std::cout << debugLine << std::endl;
-    // }
-
-    // debugFile.close();
-
+    // deleting the objects remained in the game board at the end
     std::set<Cell*> uniqueObjects;
-
     for (int y = 0; y < board.getHeight(); ++y) {
         for (int x = 0; x < board.getWidth(); ++x) {
             const CellSlot& slot = board.getSlot(x, y);
             const auto& objects = slot.getAll();
             for (Cell* obj : objects) {
-                uniqueObjects.insert(obj); // נכנס רק פעם אחת לכל אובייקט
+                uniqueObjects.insert(obj); // only one time for each object
             }
         }
     }
-
-    // חשוב: לא לכלול Shells כי הם כבר נמחקו על ידי GameManager
+    // GameManager itself deletes the shells, so we delete only objects that are not shells
     for (Cell* obj : uniqueObjects) {
         if (dynamic_cast<Shell*>(obj) == nullptr) {
             delete obj;
         }
     }
-    Logger::shutdown(); // סגירת קובץ הלוג
-
-
-
+    Logger::shutdown(); // closing the logger
     return 0;
 }
